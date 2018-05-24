74842ddc46e119ca8a8b043acd0f4fbca7d013c5
Gilgalad <notsuperman@mail.ru>
2018-05-24 16:40:56 +0300

    add JSPROJECT folder

---
+++ JSPROJECT/js_sandbox/app.js
@@ -0,0 +1,35 @@
+console.log("Hello World");
+console.log("123");
+console.log("true");
+var greeting = 'Hello'; 
+console.log(greeting);
+console.log([1,2,3,4,5]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+console.error('My Error message');
+console.clear();
+console.warn('My Warn');
+console.time('Hello');
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.timeEnd('Hello');
+console.clear();
+// var, let, const - объявление переменных
+var name = 'John Doe';
+console.log(name);
+name = 'Steve';
+console.log(name);
+
+var greeting2;
+console.log(greeting2);
+greeting2 = 'Hello';
+console.log(greeting2);
+
+// название переменной может содержать только такие символы: буквы, числа, _, $.
+//переменная не может начинаться с числа
+
+// Multi word vars
+var firstName = 'John'; // Camel Case REOMENDED
+var first_Name = 'John'; // Underscore
+var FirstName = 'John'; // Pascal Case SOMETIMES
